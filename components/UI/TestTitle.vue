be0c71b540edf4bc929eb79073a25c9ce916a018
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-07-21 04:17:59 +0100

    title animation

---
+++ components/UI/TestTitle.vue
@@ -0,0 +1,21 @@
+<template>
+    <h1 class="randomtext">{{text}}</h1>
+</template>
+
+<script setup>
+ const props = defineProps(
+    {
+        text: {
+            type: String,
+            required: true,
+        },
+    }
+ )
+</script>
+
+
+<style scoped>
+h1 {
+     @apply text-[3.75rem];
+}
+</style>
